cba2ae88b66b6d141f067699dd8f24deb1b9c7be
BrunoDefalque <defalquebruno@gmail.com>
2023-04-18 09:20:05 +0200

    Sort.java

---
+++ jeux/Sort.java
@@ -0,0 +1,17 @@
+package jeux;
+
+/**
+ * Sort
+ */
+public class Sort {
+
+	/ /Propriétés :
+
+	private String nom, description;
+
+	private int niveauObtention, degatMin, degatMax, coutMana;
+
+	private boolean isDegat;
+
+	
+}
